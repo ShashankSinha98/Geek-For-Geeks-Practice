937a47bf6da18bb141989921b44a1b8e293fd5d2
Shashank Sinha <34626597+ShashankSinha98@users.noreply.github.com>
2020-08-21 00:19:09 +0530

    Added Code

---
+++ Strings/1. Longest Prefix Suffix.py
@@ -0,0 +1,33 @@
+t = int(input())
+
+def longest_prefix_suffix(inp_str):
+
+    n = len(inp_str)
+    arr = [0]*n
+
+    i = 1
+    j = 0
+
+    while i<n:
+
+        if inp_str[i] == inp_str[j]:
+            arr[i] = j+1
+            i+=1
+            j+=1
+
+        else:
+            if j==0:
+                arr[j] = 0
+                i+=1
+            else:
+                j = arr[j-1]
+
+    return max(arr)
+
+
+
+while t!=0:
+    t-=1
+
+    inp_str = input()
+    print(longest_prefix_suffix(inp_str))
